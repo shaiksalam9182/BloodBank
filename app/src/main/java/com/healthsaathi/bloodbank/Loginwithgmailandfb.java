621d173ac4cef82d6ff14d77ac8a9d0284a39701
shaiksalam9182 <shaiksalam9182@gmail.com>
2017-06-13 16:08:52 -0800

    Third commit

--- app/src/main/java/com/healthsaathi/bloodbank/Loginwithgmailandfb.java
+++ app/src/main/java/com/healthsaathi/bloodbank/Loginwithgmailandfb.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -18,32 +25,58 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,View.OnClickListener{
 
-    Button gmaillogin;
+
 
     FirebaseAuth mauth;
     GoogleApiClient mapiclient;
     FirebaseAuth.AuthStateListener mauthlistener;
     FirebaseDatabase mdatabase;
+    private CallbackManager mcallmangaer;
     DatabaseReference mdatabasereference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_loginwithgmailandfb);
 
 
         mdatabase = FirebaseDatabase.getInstance();
         mdatabasereference = mdatabase.getReference();
 
-        gmaillogin = (Button)findViewById(R.id.bgmaillogin);
+
+        findViewById(R.id.login_button).setOnClickListener(this);
+        findViewById(R.id.bgmaillogin).setOnClickListener(this);
+        mcallmangaer = CallbackManager.Factory.create();
+
+        LoginButton loginbutton = (LoginButton)findViewById(R.id.login_button);
+        loginbutton.setReadPermissions("email","public_profile");
+        loginbutton.registerCallback(mcallmangaer, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleaccesstoken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(Loginwithgmailandfb.this,"Cancelled",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(Loginwithgmailandfb.this,"Erro Occured\nTray Again Later",Toast.LENGTH_LONG).show();
+
+            }
+        });
 
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -64,7 +97,7 @@ public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiC
                 final FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user!=null){
                     Toast.makeText(Loginwithgmailandfb.this,user.getEmail(),Toast.LENGTH_LONG).show();
-                    gmaillogin.setText(user.getEmail());
+                    //gmaillogin.setText(user.getEmail());
                 }else {
                     Toast.makeText(Loginwithgmailandfb.this,"Signed Out",Toast.LENGTH_LONG).show();
                 }
@@ -74,8 +107,25 @@ public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiC
 
     }
 
+    private void handleaccesstoken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mauth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Loginwithgmailandfb.this,"Status"+task.isSuccessful(),Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(Loginwithgmailandfb.this,"Status"+task.getException(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 
-    public void signinwithgmail(View v) {
+
+
+
+    public void signinwithgmail() {
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(mapiclient);
         startActivityForResult(intent,9001);
     }
@@ -83,13 +133,19 @@ public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiC
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        mcallmangaer.onActivityResult(requestCode,resultCode,data);
         if (requestCode==9001){
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()){
                 GoogleSignInAccount account = result.getSignInAccount();
                 authwithfirebase(account);
+            }else {
+
             }
         }
+
+
+
     }
 
     private void authwithfirebase(final GoogleSignInAccount account) {
@@ -100,7 +156,7 @@ public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiC
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             Toast.makeText(Loginwithgmailandfb.this,"Status:"+task.isSuccessful(),Toast.LENGTH_LONG).show();
-                            gmaillogin.setText(account.getEmail());
+
                         }else {
                             Toast.makeText(Loginwithgmailandfb.this,"Status:"+task.getException(),Toast.LENGTH_LONG).show();
                         }
@@ -118,4 +174,17 @@ public class Loginwithgmailandfb extends AppCompatActivity implements GoogleApiC
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Toast.makeText(Loginwithgmailandfb.this,"Google Play Services Error",Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        switch (i){
+            case R.id.bgmaillogin:
+                signinwithgmail();
+                break;
+
+        }
+    }
+
+
 }
